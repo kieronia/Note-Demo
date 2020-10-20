eec641f059a63feea1e7acc6e1a2a702a3b0688b
Kieronia <70661699+kieronia@users.noreply.github.com>
2020-10-20 17:21:57 +0100

    Update MassNote.py

--- MassNote.py
+++ MassNote.py
@@ -4,7 +4,7 @@ message = input(" > Note message?\n > ")
 headers = {'Authorization': token}
 resp = requests.get("https://discord.com/api/v8/users/@me/relationships",headers=headers)
 data = json.loads(resp.text)
-deleted = int(0)
+
 
 for i in range(len(data)):
     print(f" + https://discordapp.com/api/v8/users/@me/notes/{data[i]['id']}")
